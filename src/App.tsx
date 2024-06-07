3578de3762b826fe029f2e0dfa5db872efcdc593
Mohammed Nasir <mohammed.nasir@ntegra.co.uk>
2024-06-07 15:10:56 +0100

    setup vite with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen flex flex-col items-center justify-center">
+      <h1 className=" text-red-500">TEST</h1>
+    </div>
+  );
+}
+
+export default App;
