7dbd0ff70648ff62303c63f93f38af59ca6a974f
Mohammed Nasir <mohammed.nasir@ntegra.co.uk>
2024-06-10 17:04:26 +0100

    have basic editable table

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,149 @@
 // import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
+import mockData from "./mockData.json";
+import {
+  useReactTable,
+  getCoreRowModel,
+  ColumnDef,
+  RowData,
+  flexRender,
+} from "@tanstack/react-table";
+
+type TDateTime = {
+  date: string;
+  time1: string;
+  time2: string;
+  time3: string;
+  time4: string;
+};
+
+declare module "@tanstack/react-table" {
+  interface TableMeta<TData extends RowData> {
+    updateData: (rowIndex: number, columnId: string, value: unknown) => void;
+  }
+}
+
+// Give our default column cell renderer editing superpowers!
+const defaultColumn: Partial<ColumnDef<TDateTime>> = {
+  cell: ({ getValue, row: { index }, column: { id }, table }) => {
+    const initialValue = getValue();
+    // We need to keep and update the state of the cell normally
+    const [value, setValue] = React.useState(initialValue);
+
+    // When the input is blurred, we'll call our table meta's updateData function
+    const onBlur = () => {
+      table.options.meta?.updateData(index, id, value);
+    };
+
+    // If the initialValue is changed external, sync it up with our state
+    React.useEffect(() => {
+      setValue(initialValue);
+    }, [initialValue]);
+
+    return (
+      <input
+        value={value as string}
+        onChange={(e) => setValue(e.target.value)}
+        onBlur={onBlur}
+      />
+    );
+  },
+};
+
 function App() {
+  const columns: ColumnDef<TDateTime>[] = React.useMemo(
+    () => [
+      {
+        header: "Date",
+        accessorKey: "date",
+      },
+      {
+        header: "First Time",
+        accessorKey: "time1",
+      },
+      {
+        header: "Second Time",
+        accessorKey: "time2",
+      },
+      {
+        header: "Third Time",
+        accessorKey: "time3",
+      },
+      {
+        header: "Fourth Time",
+        accessorKey: "time4",
+      },
+    ],
+    []
+  );
+
+  const [data, setData] = React.useState(mockData);
+
+  const table = useReactTable({
+    data,
+    columns,
+    defaultColumn,
+    getCoreRowModel: getCoreRowModel(),
+    // Provide our updateData function to our table meta
+    meta: {
+      updateData: (rowIndex, columnId, value) => {
+        setData((old) =>
+          old.map((row, index) => {
+            if (index === rowIndex) {
+              return {
+                ...old[rowIndex]!,
+                [columnId]: value,
+              };
+            }
+            return row;
+          })
+        );
+      },
+    },
+    debugTable: true,
+  });
   return (
     <div className="w-screen flex flex-col items-center justify-center">
       <h1 className=" text-red-500">TEST</h1>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id}>
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            console.log("row", row);
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
